1ef96d1b690462eb6d19da95926e66d7dc41e3c7
Yuecui Li <liyuecui@cn.ibm.com>
2017-07-27 17:43:21 +0800

    add animation for change page.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,31 +7,36 @@ document.addEventListener('touchmove', function (event) {
 }, false);
 
 var curpage=1;
-var totalpage,nextpage;
+var totalpage,nextpage,lastpage,nexttotalpage;
 
 $(document).swipeUp(function(){
 
 //判断当前页是否为最后一页
 
-        //获取总页数
+        //获取总页数,以及总页数的+1后的序号，供后面for循环使用
     totalpage = $(".page").length;
-    //如果是最后一页，显示第一页，否则显示下一页
+    nexttotalpage = totalpage+1;
+    //如果是最后一页，显示第一页，并移除所有page上的所有css效果类，否则显示下一页，并移除上一页的切换动画效果
     if(curpage == totalpage){
-       $(".page1").removeClass("hide");
+        for(var i=1; i<nexttotalpage;i++){
+            $(".page"+i).removeClass("hide");
+            $(".page"+i).removeClass("show");
+            $(".page"+i).removeClass("flip-bottom");
+        }
         $(".page1").addClass("show");
-        $(".page1").addClass("pt-page-moveFromBottom");
-        $(".page"+curpage).removeClass("show");
-        $(".page"+curpage).addClass("hide");
-        $(".page"+curpage).addClass("pt-page-moveToTop");
+        $(".page1").addClass("flip-bottom");
         curpage =1;
     }else{
         nextpage = curpage+1;
-        $(".page"+nextpage).removeClass("hide");
-        $(".page"+nextpage).addClass("show");
-        $(".page"+nextpage).addClass("pt-page-moveFromBottom");
+        lastpage = curpage-1;
+        $(".page"+curpage).removeClass("flip-bottom");
+        $(".page"+curpage).addClass("flip-top");
         $(".page"+curpage).removeClass("show");
         $(".page"+curpage).addClass("hide");
-        $(".page"+curpage).addClass("pt-page-moveToTop");
+        $(".page"+nextpage).removeClass("hide");
+        $(".page"+nextpage).addClass("show");
+        $(".page"+nextpage).addClass("flip-bottom");
+        $(".page"+lastpage).removeClass("legend-top");
         curpage = nextpage;
     }
 });
