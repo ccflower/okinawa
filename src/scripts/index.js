a0beb9c98530e8dc5782230174d95e8a3118a110
ccflower <lyc.dream@163.com>
2017-10-23 13:45:44 +0800

    refactor the logic of switch page. Add regret machanism.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -38,51 +38,28 @@ $(function() {
 
     //logic for page switch
     var curpage=1;
-    var totalpage, nextpage, prepage, lastpage, nexttotalpage, pretotalpage;
+    var totalpage, nextpage, lastpage;
     //获取总页数,以及总页数的+1后的序号，供后面for循环使用
     totalpage = $(".page").length;
-    console.log(totalpage);
-    nexttotalpage = totalpage + 1;
-    pretotalpage = totalpage - 1;
 
     function switchPage(toward) {
-        //如果是最后一页，显示第一页，并移除所有page上的所有css效果类，否则显示下一页，并移除上一页的切换动画效果
-        if(curpage == totalpage && nextpage != pretotalpage){
-            for(var i=1; i<nexttotalpage;i++){
-                $(".page"+i).removeClass("hide");
-                $(".page"+i).removeClass("show");
-                $(".page"+i).removeClass("flip-bottom");
-            }
-            $(".page1").addClass("show");
-            $(".page1").addClass("flip-bottom");
-            //为文字添加随机特效
-            clearTextAnimation();
-            var randomNum = rnd(0,temLength);
-            setTimeout(function(){
-                $(".textbox"+1).css('display','block');
-                $(".textbox"+1).addClass(inClassArray[0]);
-            },500);
-            curpage =1;
-        }else{
             if(toward=="up"){
-                console.log("up");
                 nextpage = curpage + 1;
                 lastpage = curpage - 1;
-                console.log('lastpage: ',lastpage );
-                console.log('curpage: ',curpage );
-                console.log('nextpage: ',nextpage ); 
+                if(curpage == totalpage) {
+                    nextpage = 1;
+                }
+                if(curpage == 1) {
+                    lastpage = totalpage;
+                }
                 var inClass ="flip-bottom";
                 var outClass="flip-top";
             }else if(toward=="down"){
                 if(curpage == 1){
                     return false;
                 }
-                console.log("down");
-                nextpage = curpage - 1;
-                lastpage = curpage + 1;
-                console.log('lastpage: ',lastpage );
-                console.log('curpage: ',curpage );
-                console.log('nextpage: ',nextpage );          
+                nextpage = curpage - 1; 
+                lastpage = curpage + 1;         
                 var outClass ="flip-top";
                 var inClass="flip-bottom";
             }
@@ -93,16 +70,16 @@ $(function() {
             $(".page" + nextpage).removeClass("hide");
             $(".page" + nextpage).addClass("show");
             $(".page" + nextpage).addClass(inClass);
+            $(".page" + lastpage).removeClass(outClass);
             //为文字添加随机特效
             clearTextAnimation();
             var randomNum = rnd(0,temLength);
             setTimeout(function(){
                 $(".textbox"+nextpage).css('display','block');
                 $(".textbox"+nextpage).addClass(inClassArray[curpage-1]);
-            },500);
-            $(".page"+lastpage).removeClass("flip-top");
+            },300);
+            
             curpage = nextpage;
-        }
     }
 
     function clearTextAnimation() {
@@ -111,10 +88,6 @@ $(function() {
             $(this).attr('class', 'textbox'+(i+1));
         });
     }
-
-    // $(document).swipeUp(function(){
-    //     switchPage();
-    // });
 
     $(document).swipeUp(function(){
         switchPage("up");
